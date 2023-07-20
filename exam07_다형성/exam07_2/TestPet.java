d6251df842987e5985181c19611794a6376d2bbf
김문정 <98167564+milkteabread@users.noreply.github.com>
2023-07-20 17:56:59 +0900

    Add files via upload

---
+++ exam07_다형성/exam07_2/TestPet.java
@@ -0,0 +1,24 @@
+package exam07_2;
+
+public class TestPet {
+
+	public static void main(String[] args) {
+		
+		// 1. 일반 객체 생성
+		Cat c1 = new Cat("툥냥",2);
+		Dog d1 = new Dog("빵티즈",1,"수컷");
+		Bird b1 = new Bird("토토",1,"옐로우그린");
+		
+		System.out.println(c1);
+		System.out.println(d1);
+		System.out.println(b1);
+		
+//		System.out.printf("Cat 이름: %s, Cat 나이: %d \n", c1.getName(), c1.getAge());
+//		System.out.printf("Dog 이름: %s, Dog 나이: %d, Dog 성별: %s \n", d1.getName(), d1.getAge(),d1.getSex());
+//		System.out.printf("Bird 이름: %s, Bird 나이: %d, Bird 색상: %s \n", b1.getName(), b1.getAge(),b1.getColor());
+		
+	
+
+	}
+
+}
