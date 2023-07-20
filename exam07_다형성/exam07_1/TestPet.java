e954fb7c0329232147dc94946e3403553634cdff
김문정 <98167564+milkteabread@users.noreply.github.com>
2023-07-20 17:55:56 +0900

    Add files via upload

---
+++ exam07_다형성/exam07_1/TestPet.java
@@ -0,0 +1,16 @@
+package exam07_1;
+
+public class TestPet {
+
+	public static void main(String[] args) {
+		
+
+		Cat c = new Cat("툥냥",2);
+		
+		System.out.println(c); // exam07_1.Cat@15db9742
+		System.out.println(c.toString());  // exam07_1.Cat@15db9742
+		
+		
+	}
+
+}
